2cf990c82666141df4e129d565d93314b06987cd
adiouma <adiouma10@gmail.com>
2024-04-19 15:02:25 +0100

    devops-sre-course-labs-step-7-v8

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 """
-
+Continuous Integration exemple app
 """
 
 from flask import Flask, request, render_template
 
 
-APP = Flask(__name__,template_folder='templates')
+APP = Flask(__name__, template_folder='templates')
 
 
 @APP.route('/')
@@ -17,15 +17,15 @@ def home():
     return render_template('index.html')
 
 
-@APP.route('/hello',methods=['POST'])
+@APP.route('/hello', methods=['POST'])
 def hello():
     '''
     For rendering results on HTML GUI
     '''
-    inputs = [x for x in request.form.values()]
+    inputs = list(request.form.values())
     result = " Hello There " + inputs[0]
-    return render_template('index.html', hello_text='{}'.format(result))
+    return render_template('index.html', hello_text=f'{result}'.format(result))
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0',port=8080)
+    APP.run(host='0.0.0.0', port=8080)
